aebd80d8f886554dbc94bb5cb926fe81c9885851
Tom Siemion <tsiemion07@gmail.com>
2021-06-06 16:08:52 -0500

    project data object

---
+++ client/src/assets/projectdata.js
@@ -0,0 +1,42 @@
+const projectData = [
+    {
+        name: 'Track My Workout',
+        description: 'A workout tracking application which also displays some data in charts for the user to stay on track with their fitness goals.',
+        tech: 'MongoDB, Express',
+        image: './assets/images/fitnesstracker1.png',
+        link: 'https://fitnesstrackerwoot.herokuapp.com/',
+        github: 'https://github.com/rotosti/TrackMyWorkout'
+    }, {
+        name: 'DrinkMyWeather',
+        description: `A web application which recommends adult beverages to the user using their
+        location's current weather.  The user can enter in a US based ZIP and get some
+        recommendations with our proprietary recommendation engine!`,
+        tech: 'Bulma, Javascript',
+        image: './assets/images/DMW-Screen.png',
+        link: 'https://rotosti.github.io/DrinkMyWeather/',
+        github: 'https://github.com/rotosti/DrinkMyWeather'
+    },{
+        name: 'User Directory',
+        description: 'An application made with React to sort through and search through state data.',
+        image: './assets/images/user-directory.png',
+        tech: 'React, Javascript, Axios',
+        link: 'https://rotosti.github.io/user-directory/',
+        github: 'https://github.com/rotosti/user-directory'
+    }, {
+        name: 'TPtoYou',
+        description: `A simple subscription service for toilet paper.  Project was for understanding log in/log out, 
+        databases, and user sessions.`,
+        image: './assets/images/user-directory.png',
+        tech: 'Express, Node, MySQL, UIKit, handlebars',
+        link: 'https://tptoyou.herokuapp.com/',
+        github: 'https://github.com/rotosti/TPtoU'
+    },{
+        name: 'My Local Weather',
+        description: 'An application using the Open Weather API to get weather based on city searches.',
+        tech: 'Javascript, OpenWeatherAPI',
+        image: './assets/images/user-directory.png',
+        link: 'https://rotosti.github.io/MyLocalWeather/',
+        github: 'https://github.com/rotosti/MyLocalWeather'   
+    }]
+
+export default projectData;
