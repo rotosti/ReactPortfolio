dfecb09eb3ab46a62bf199da0ccb03d8718318ac
Tom Siemion <tsiemion07@gmail.com>
2021-06-06 18:59:08 -0500

    updated project image routes

--- client/src/assets/projectdata.js
+++ client/src/assets/projectdata.js
@@ -3,7 +3,7 @@ const projectData = [
         name: 'Track My Workout',
         description: 'A workout tracking application which also displays some data in charts for the user to stay on track with their fitness goals.',
         tech: 'MongoDB, Express',
-        image: './assets/images/fitnesstracker1.png',
+        image: './images/fitnesstracker1.png',
         link: 'https://fitnesstrackerwoot.herokuapp.com/',
         github: 'https://github.com/rotosti/TrackMyWorkout'
     }, {
@@ -12,13 +12,13 @@ const projectData = [
         location's current weather.  The user can enter in a US based ZIP and get some
         recommendations with our proprietary recommendation engine!`,
         tech: 'Bulma, Javascript',
-        image: './assets/images/DMW-Screen.png',
+        image: './images/DMW-Screen.png',
         link: 'https://rotosti.github.io/DrinkMyWeather/',
         github: 'https://github.com/rotosti/DrinkMyWeather'
     },{
         name: 'User Directory',
         description: 'An application made with React to sort through and search through state data.',
-        image: './assets/images/user-directory.png',
+        image: './images/user-directory.png',
         tech: 'React, Javascript, Axios',
         link: 'https://rotosti.github.io/user-directory/',
         github: 'https://github.com/rotosti/user-directory'
@@ -26,7 +26,7 @@ const projectData = [
         name: 'TPtoYou',
         description: `A simple subscription service for toilet paper.  Project was for understanding log in/log out, 
         databases, and user sessions.`,
-        image: './assets/images/user-directory.png',
+        image: './images/TPtoYou.png',
         tech: 'Express, Node, MySQL, UIKit, handlebars',
         link: 'https://tptoyou.herokuapp.com/',
         github: 'https://github.com/rotosti/TPtoU'
@@ -34,7 +34,7 @@ const projectData = [
         name: 'My Local Weather',
         description: 'An application using the Open Weather API to get weather based on city searches.',
         tech: 'Javascript, OpenWeatherAPI',
-        image: './assets/images/user-directory.png',
+        image: './images/MyLocalWeather-1.png',
         link: 'https://rotosti.github.io/MyLocalWeather/',
         github: 'https://github.com/rotosti/MyLocalWeather'   
     }]
